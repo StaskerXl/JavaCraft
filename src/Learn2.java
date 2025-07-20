4a40665f3a3440435539d6f16c6301cf345149ce
StaskerXl <lozhkin_Stas@Mail.ru>
2025-07-21 01:35:08 +0300

    This is new learning java development from my live

---
+++ src/Learn2.java
@@ -0,0 +1,41 @@
+public class Learn2 {
+    public static void main(String[] args) {
+
+        //Comment
+
+        int sizeTea = 1000;
+
+        System.out.println(sizeTea);
+
+        double TestScore = 10.0;
+        boolean isCool = true;
+        String name = "Cast";
+
+        System.out.println(name + ", is he cool? " + isCool);
+
+        int bob;
+
+        bob = 45;
+
+        System.out.println(bob);
+
+        double newScore = 56.0;
+
+        System.out.println(newScore);
+
+        newScore = 100.0;
+
+        System.out.println(newScore);
+
+        int x, y, z;
+
+        x = 3;
+        y = 2;
+        z = 10;
+
+        int $TSLA = 450;
+
+
+
+    }
+}
