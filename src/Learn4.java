8b9051b4dcae3916329d4fb4e1202523bba99870
StaskerXl <lozhkin_Stas@Mail.ru>
2025-07-23 00:30:13 +0300

    This is new learning java development from my live

---
+++ src/Learn4.java
@@ -0,0 +1,34 @@
+public class Learn4 {
+    public static void main(String[] args) {
+
+        int number = 4;
+        int resault;
+
+        resault = number + 1;
+        System.out.println(number);
+
+        number += 1;
+        System.out.println(number);
+
+        number -= 1;
+        System.out.println(number);
+
+        number /= 2;
+        System.out.println(number);
+
+        number *= 2;
+        System.out.println(number);
+
+        number %= 2;
+        System.out.println(number);
+
+        number++;
+        System.out.println(number);
+
+        number--;
+        System.out.println(number);
+
+
+
+    }
+}
